6888fecaee3a633a790cd89c0ea3857927491b24
Ricardo061001 <ricardo.villaroel@educa.madrid.org>
2024-04-02 22:58:52 +0200

    contructor de la clase equipo

--- src/futbol/Equipo.java
+++ src/futbol/Equipo.java
@@ -5,13 +5,22 @@ package futbol;
  * @author Ricardo
  * @version 1.0
  * @since 02/04/2024
+ * @see Jugador
  */
 public class Equipo {
     // Atributos
     private String nombre;
     private Jugador[] jugadores;
 
-
+    /**
+     * Constructor de la clase Equipo
+     * @param nombre El nombre del equipo
+     * @param jugadores Los jugadores del equipo
+     */
+    public Equipo(String nombre, Jugador[] jugadores) {
+        this.nombre = nombre;
+        this.jugadores = jugadores;
+    }
 
 
 
