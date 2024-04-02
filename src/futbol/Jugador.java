93304e9bfc6abe41874a07d8f929be33de43e609
Ricardo061001 <ricardo.villaroel@educa.madrid.org>
2024-04-02 23:01:29 +0200

    añadimos los atributos

--- src/futbol/Jugador.java
+++ src/futbol/Jugador.java
@@ -10,32 +10,10 @@ public class Jugador {
     private String nombre;
     private int edad;
 
-    /**
-     * Constructor de la clase Jugador
-     * @param nombre El nombre del jugador
-     * @param edad La edad del jugador
-     */
-    public Jugador(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
 
-    // Métodos
-    /**
-     * Método para obtener el nombre del jugador
-     * @return El nombre del jugador
-     */
-    public String getNombre() {
-        return this.nombre;
-    }
 
-    /**
-     * Método para obtener la edad del jugador
-     * @return La edad del jugador
-     */
-    public int getEdad() {
-        return this.edad;
-    }
+
+
 }
 
 
